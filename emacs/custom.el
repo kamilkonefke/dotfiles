d5078735fb338452f1f0df399ed44ef759af99b0
Kamil Konefke <kamilkonefkee@gmail.com>
2026-04-25 00:05:24 +0200

    refactor: more 'modern' emacs config

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,15 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(gruber-darker))
+ '(custom-enabled-themes '(nord))
  '(custom-safe-themes
-   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+   '("5a4cdc4365122d1a17a7ad93b6e3370ffe95db87ed17a38a94713f6ffe0d8ceb"
+     "2741a783402094880614410201f3fd3b4f4999bdae715bef16dd5d3b395a82ff"
+     "877f16ec78dd07fdbc6c49cd017806cc815ad0dfb8e7a9bd1c64e7430c5faa5e"
+     "4f0eae1854b3bcf8b34ea4fceab165580e3edcfd59d35a40a4a4590459b4c932"
+     "ed234dfada912bfbe03b419874fed10980cdb71f4ef72ea848e0e10377363d20"
+     "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
      "183bfd1efb07b9001c90e76f30241975ab067bc971d2567276cd1e6867ca58d3"
      default))
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(## darkokai-theme gruber-darker-theme gruvbox-theme magit move-text
-        multiple-cursors smex)))
+   '(corfu magit marginalia multiple-cursors nord-theme orderless
+           rust-mode vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
